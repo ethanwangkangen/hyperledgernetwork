a8a36d75613c02ac809025045a17f58a69b60495
Ethan Wang <ethanwangkangen@gmail.com>
2025-02-02 23:44:39 +0800

    Initial commit

---
+++ network.sh
@@ -0,0 +1,74 @@
+# This script brings up the network.
+
+
+#!/bin/bash
+
+# Set PATH to include the local bin directory
+export PATH=${PWD}/bin:$PATH
+export FABRIC_CFG_PATH=$PWD/config/
+
+
+# Function to start the network
+function startNetwork() {
+    echo "Starting the network..."
+
+    # generate artifacts if they don't exist
+    if [ ! -d "organizations/peerOrganizations" ]; then
+        generateCrypto
+    fi
+
+    docker-compose -f docker-compose.yaml up -d
+
+    echo "Network started."
+}
+
+# Function to stop the network
+function stopNetwork() {
+    echo "Stopping the network..."
+
+    # Stop and remove containers, networks, volumes, and images created by 'docker-compose up'
+    docker-compose -f docker-compose.yaml down --volumes --remove-orphans
+
+    # Ensure that all containers are stopped and removed
+    docker rm -f $(docker ps -aq)  # Force remove all containers
+
+    # Ensure that all unused networks are removed
+    docker network prune -f
+
+    # Ensure that all unused volumes are removed
+    docker volume prune -f
+
+    # Ensure that all unused images are removed
+    docker rmi -f $(docker images -q)
+    echo "Network stopped and cleaned up."
+}
+
+
+# Function to generate cryptographic material
+function generateCrypto() {
+    echo "Generating cryptographic material..."
+    cryptogen generate --config=./crypto-config.yaml --output="crypto-config"
+    echo "Cryptographic material generated."
+}
+
+# Function to create genesis block/create application channel
+function generateGenesis() {
+    echo "Generating genesis block for channel..."
+    configtxgen -profile ChannelUsingRaft -outputBlock ./channel-artifacts/genesis.block -channelID myblock
+    configtxgen -profile MyChannel -outputCreateChannelTx ./channel-artifacts/mychannel.tx -channelID mychannel
+
+}
+
+
+# Main entry point: check for the first argument ("up" or "down")
+if [ "$1" == "createChannel" ]; then
+    generateGenesis
+fi
+if [ "$1" == "up" ]; then
+    startNetwork
+elif [ "$1" == "down" ]; then
+    stopNetwork
+else
+    echo "Usage: $0 {up|down}"
+    exit 1
+fi
