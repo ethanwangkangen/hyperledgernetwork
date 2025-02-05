66317ba1594e166cd0fb4669c6a67472e1e9cb51
ethan wang <ethanwangkangen@gmail.com>
2025-02-05 16:54:19 +0800

    Package chaincode

--- channeljoinscripts/joinPeer0.org1.sh
+++ channeljoinscripts/joinPeer0.org1.sh
@@ -10,8 +10,8 @@ export CORE_PEER_TLS_CERT_FILE="crypto-config/peerOrganizations/org1.example.com
 export CORE_PEER_TLS_KEY_FILE="crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/server.key"
 export CORE_PEER_ADDRESS=localhost:11051
 export CORE_PEER_MSPCONFIGPATH="crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp"
-export CORE_PEER_LOGGING=DEBUG
-export FABRIC_LOGGING=debug
+export CORE_PEER_LOGGING=INFO
+export FABRIC_LOGGING=INFO
 
 # Create the channel by passing the transaction file
 if peer channel create -o localhost:7049  --ordererTLSHostnameOverride orderer.example.com -c mychannel -f ../channel-artifacts/mychannel.tx --outputBlock ../channel-artifacts/mychannel.block --tls --cafile "./crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" ; then
