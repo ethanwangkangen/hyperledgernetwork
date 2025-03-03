f9f86d38fec5c9889788bca95e6ec10f5dfa900d
ethan wang <ethanwangkangen@gmail.com>
2025-03-03 17:11:26 +0800

    Add debug lines

--- networks/network_bft/test.sh
+++ networks/network_bft/test.sh
@@ -1,5 +1,5 @@
 
-source ./setPeerEnv.sh org1 peer5
+source ./setPeerEnv.sh org1 peer0
 peer lifecycle chaincode queryapproved -n mychaincode
 peer chaincode invoke -o localhost:7049 --ordererTLSHostnameOverride orderer.example.com \
 	--tls --cafile "${PWD}/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" \
@@ -7,4 +7,4 @@ peer chaincode invoke -o localhost:7049 --ordererTLSHostnameOverride orderer.exa
 	-n mychaincode \
 	--peerAddresses localhost:11054 --tlsRootCertFiles "${PWD}/crypto-config/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt" \
 	--peerAddresses localhost:13051 --tlsRootCertFiles "${PWD}/crypto-config/peerOrganizations/org1.example.com/peers/peer2.org1.example.com/tls/ca.crt" \
-	-c '{"Args":["createAsset", "asset1", "value1"]}'
+	-c '{"Args":["createAsset", "asset1", "value1 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"]}'
